633d12a2ecc25d19d03f69f4879ffc944371ba5a
Simon Jäggi <dev.simon.jaeggi@gmail.com>
2025-01-29 16:03:25 +0100

    Initial commit

---
+++ src/example.test.js
@@ -0,0 +1,12 @@
+test('Test case 1', ()=>{
+    expect(2).toBe(2);
+})
+test('Test case 2', ()=>{
+    expect(2).toBe(2);
+})
+test('Test case 3', ()=>{
+    expect(3).toBe(3);
+})
+test('Test case 4', ()=>{
+    expect(3).toBe(3);
+})
