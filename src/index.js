89f29b5e7ac30a030fd7147e8f8a5119075de7ce
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-01-30 16:03:56 +0100

    added interactivity to searchbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
 import "./styles.css";
-import { getLocationWeather } from "./weather";
+import { fetchWeather } from "./weather";
+import { populateDailyWeather } from "./populateDailyWeather";
+import { populateHourlyWeather } from "./populateHourlyWeather";
 
-getLocationWeather();
+const content = document.querySelector(".content");
+const searchbarBtn = document.querySelector("#searchbar_button");
+const searchbar = document.querySelector("#searchbar");
 
+const populatePage = (location, unitGroup) => {
+    fetchWeather(location, unitGroup, "hours")
+        .catch((error) => {
+            console.error("Error fetching weather data:", error.message);
+        })
+        .then((data) => {
+            if (data) {
+                populateHourlyWeather(data);
+                populateDailyWeather(data);
+                searchbar.value = data.resolvedAddress;
+            } else {
+                alert(
+                    `Location "${location}" not found. Please check the spelling.`
+                );
+            }
+        });
+};
+
+const purgePage = () => {
+    content.replaceChildren();
+};
+
+
+const search = () => {
+    const trimmedLocation = searchbar.value.trim()
+    if(trimmedLocation){
+        purgePage();
+        populatePage(trimmedLocation, "metric"); 
+    }else{
+        alert("Please enter a location name.");
+    }
+};
+
+searchbarBtn.addEventListener("click", search);
+document.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    searchbarBtn.click();
+  }
+});
