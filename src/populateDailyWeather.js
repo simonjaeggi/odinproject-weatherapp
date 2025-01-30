173e9d881dd9ff2aab15a7c3f2f7b47d05364b2e
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-01-30 16:01:49 +0100

    implemented hourly and daily weather visualisation

---
+++ src/populateDailyWeather.js
@@ -0,0 +1,69 @@
+const icons = require.context("./icons", false, /\.(jpg|jpeg|png|gif|svg)$/);
+const content = document.querySelector(".content");
+
+const populateDailyWeather = (data) => {
+    const daysAfterToday = data.days.splice(1);
+
+    daysAfterToday.forEach((day) => {
+        const dailyContainer = document.createElement("div");
+        dailyContainer.classList.add("card", "day_container");
+
+        const date = document.createElement("span");
+        const icon = document.createElement("img");
+
+        date.textContent = new Date(day.datetime).toLocaleDateString("de-CH");
+        date.classList.add("title");
+        icon.src = icons(`./${day.icon}.svg`);
+
+        const description = document.createElement("span");
+        description.classList.add("weather_description");
+        description.textContent = day.description;
+
+        const infoContainer = document.createElement("div");
+        infoContainer.classList.add("info_container");
+
+        const tempLabel = document.createElement("span");
+        const feltTempLabel = document.createElement("span");
+        const precipLabel = document.createElement("span");
+        const sunLabel = document.createElement("span");
+
+        [tempLabel, feltTempLabel, precipLabel, sunLabel].forEach((label) =>
+            label.classList.add("subtitle")
+        );
+
+        const temp = document.createElement("span");
+        const feltTemp = document.createElement("span");
+        const precipProb = document.createElement("span");
+        const sun = document.createElement("span");
+
+        tempLabel.textContent = "Temperature";
+        feltTempLabel.textContent = "Felt temperature";
+        precipLabel.textContent = "Precipitation";
+        sunLabel.textContent = "Sun timing";
+
+        temp.textContent = `Min: ${day.tempmin} °C, Avg: ${day.temp} °C, Max: ${day.tempmax} °C`;
+        feltTemp.textContent = `Min: ${day.tempmin} °C, Avg: ${day.temp} °C, Max: ${day.tempmax} °C`;
+        precipProb.textContent = `Probability: ${day.precipprob}%`;
+        sun.textContent = `Sunrise ${day.sunrise.split(":").splice(0, 2).join(":")}, Sunset ${day.sunset.split(":").splice(0, 2).join(":")}`;
+
+        [
+            tempLabel,
+            temp,
+            feltTempLabel,
+            feltTemp,
+            precipLabel,
+            precipProb,
+            sunLabel,
+            sun,
+        ].forEach((element) => infoContainer.appendChild(element));
+
+        dailyContainer.appendChild(date);
+        dailyContainer.appendChild(icon);
+        dailyContainer.appendChild(infoContainer);
+        dailyContainer.appendChild(description);
+
+        content.appendChild(dailyContainer);
+    });
+};
+
+module.exports = { populateDailyWeather };
---
+++ src/populateHourlyWeather.js
@@ -0,0 +1,46 @@
+const icons = require.context("./icons", false, /\.(jpg|jpeg|png|gif|svg)$/);
+const content = document.querySelector(".content");
+
+const populateHourlyWeather = (data) => {
+    const hourlyContainer = document.createElement("div");
+    hourlyContainer.classList.add("card");
+
+    const weatherHoursContainer = document.createElement("div");
+    weatherHoursContainer.classList.add("weather_hours");
+
+    const title = document.createElement("span");
+    title.classList.add("title");
+    title.textContent = "Hours";
+
+    hourlyContainer.appendChild(title);
+    hourlyContainer.appendChild(weatherHoursContainer);
+    content.appendChild(hourlyContainer);
+
+    const currentHour = new Date().getHours();
+    const today = data.days[0];
+    const tomorrow = data.days[1];
+    const next48Hours = Object.values(today.hours).concat(
+        Object.values(tomorrow.hours)
+    );
+    const next12Hours = next48Hours.splice(currentHour, 12);
+
+    next12Hours.forEach((hour) => {
+        const hourContainer = document.createElement("div");
+        hourContainer.classList.add("hour_container");
+
+        const icon = document.createElement("img");
+        const temp = document.createElement("span");
+        const time = document.createElement("span");
+
+        temp.textContent = `${hour.temp} °C`;
+        icon.src = icons(`./${hour.icon}.svg`);
+        time.textContent = `${hour.datetime.split(":").splice(0, 2).join(":")}`;
+
+        hourContainer.appendChild(temp);
+        hourContainer.appendChild(icon);
+        hourContainer.appendChild(time);
+
+        weatherHoursContainer.appendChild(hourContainer);
+    });
+};
+module.exports = { populateHourlyWeather };
