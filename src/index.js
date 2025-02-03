428c5549eebc1a7e954e69f300651e81a6fb018f
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-02-03 09:20:28 +0100

    replaced alerts with renderMessage

--- src/index.js
+++ src/index.js
@@ -7,42 +7,57 @@ const content = document.querySelector(".content");
 const searchbarBtn = document.querySelector("#searchbar_button");
 const searchbar = document.querySelector("#searchbar");
 
-const populatePage = (location, unitGroup) => {
-    fetchWeather(location, unitGroup, "hours")
-        .catch((error) => {
-            console.error("Error fetching weather data:", error.message);
-        })
-        .then((data) => {
-            if (data) {
-                populateHourlyWeather(data);
-                populateDailyWeather(data);
-                searchbar.value = data.resolvedAddress;
-            } else {
-                alert(
-                    `Location "${location}" not found. Please check the spelling.`
-                );
-            }
-        });
-};
+const asyncPopulatePage = async (location, unitGroup) => {
+    try {
+      const weatherData = await fetchWeather(location, unitGroup, "hours");
+      if (weatherData) {
+        populateHourlyWeather(weatherData);
+        populateDailyWeather(weatherData);
+  
+        searchbar.value = weatherData.resolvedAddress;
+      } else {
+        renderMessage(`
+                Location ${location} not found, sorryyyy 😞
+                `);
+      }
+    } catch (error) {
+      console.error("Error fetching weather data:", error.message);
+    }
+  };
 
 const purgePage = () => {
     content.replaceChildren();
 };
 
+const renderMessage = (message) => {
+    let messageContainer = document.querySelector("#messageContainer");
+    if(messageContainer){
+        messageContainer.replaceChildren();
+    }else{
+        messageContainer = document.createElement("div");
+        messageContainer.classList.add("card");
+        messageContainer.id = "messageContainer";
+    }
+    const messageSpan = document.createElement("span");
+    messageSpan.textContent = message;
+    messageContainer.appendChild(messageSpan);
+
+    content.prepend(messageContainer);
+};
 
 const search = () => {
-    const trimmedLocation = searchbar.value.trim()
-    if(trimmedLocation){
+    const trimmedLocation = searchbar.value.trim();
+    if (trimmedLocation) {
         purgePage();
-        populatePage(trimmedLocation, "metric"); 
-    }else{
-        alert("Please enter a location name.");
+        asyncPopulatePage(trimmedLocation, "metric");
+    } else {
+        renderMessage("Please enter a location name.");
     }
 };
 
 searchbarBtn.addEventListener("click", search);
 document.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") {
-    searchbarBtn.click();
-  }
+    if (event.key === "Enter") {
+        searchbarBtn.click();
+    }
 });
