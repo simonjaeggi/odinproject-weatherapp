22a653d2d6b0484d34e27fa0eb4347801bf0c698
simonjaeggi <dev.simon.jaeggi@gmail.com>
2025-02-03 09:21:39 +0100

    removed redundant try catch

--- src/weather.js
+++ src/weather.js
@@ -1,40 +1,38 @@
 async function fetchWeather(location, unitGroup, timemode) {
-  try {
     validateLocation(location);
     validateUnitGroup(unitGroup);
     validateTimemode(timemode);
-  } catch (error) {
-    return Promise.reject(error);
-  }
-  const query = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&include=${timemode}&key=${process.env.VISUALCROSSING}&contentType=json`
-  console.log(query);
-  return fetch(query)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`API request failed with status ${response.status}`);
-    }
-    return response.json(); 
-  });
+
+    const query = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&include=${timemode}&key=${process.env.VISUALCROSSING}&contentType=json`;
+    console.log(query);
+    return fetch(query).then((response) => {
+        if (!response.ok) {
+            throw new Error(
+                `API request failed with status ${response.status}`
+            );
+        }
+        return response.json();
+    });
 }
 
 function validateLocation(location) {
-  if (typeof location !== "string" || location.trim().length === 0) {
-    throw new Error("Invalid location. Must be a non-empty string.");
-  }
+    if (typeof location !== "string" || location.trim().length === 0) {
+        throw new Error("Invalid location. Must be a non-empty string.");
+    }
 }
 
 function validateUnitGroup(unitGroup) {
-  const validUnits = new Set(["metric", "us"]);
-  if (!validUnits.has(unitGroup)) {
-    throw new Error("Invalid unit group. Use 'metric' or 'us'.");
-  }
+    const validUnits = new Set(["metric", "us"]);
+    if (!validUnits.has(unitGroup)) {
+        throw new Error("Invalid unit group. Use 'metric' or 'us'.");
+    }
 }
 
 function validateTimemode(timemode) {
-  const validUnits = new Set(["days", "hours"]);
-  if (!validUnits.has(timemode)) {
-    throw new Error("Invalid timemode. Use 'days' or 'hours'.");
-  }
+    const validUnits = new Set(["days", "hours"]);
+    if (!validUnits.has(timemode)) {
+        throw new Error("Invalid timemode. Use 'days' or 'hours'.");
+    }
 }
 
 module.exports = { fetchWeather };
